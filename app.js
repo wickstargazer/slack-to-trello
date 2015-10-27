37e59448f10b324c787511c20b49921c53af497c
wickstargazer <wickyorama@gmail.com>
2015-10-27 17:53:02 +0700

    added some functions

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ function postToTrello(listId, command, text, user_name, cb) {
 	trello.post('/1/lists/' + listId + '/cards', card_data, cb);
 }
 
+function postChecklistToTrello(listId, command, text, user_name, cb) {
+
+    var name_and_desc = text.split('|');
+    var checklist_data = {
+        'name': name_and_desc.shift() + ' (@' + user_name + ')',
+        'desc': name_and_desc.shift()
+    };
+
+    trello.post('/1/lists/' + listId + '/checklists', checklist_data, cb);
+}
+
+function postChecklistItemsToTrello(listId, command, text, user_name, cb) {
+
+    var item_data = {
+        'desc': text
+    };
+    trello.post('/1/lists/' + listId + 'checklists/checkItems', item_data, cb);
+}
+
 app.post('/*', function(req, res, next) {
   var listId = req.params[0];
   var command = req.body.command,
@@ -35,13 +54,23 @@ app.post('/*', function(req, res, next) {
 
     var name = data.name;
     var url = data.shortUrl;
-
     res.status(200).send('Card "' + name + '" created here: <' + url + '>');
   });
 });
 
 // test route
 app.get('/', function (req, res) { res.status(200).send('SupportKit.io loves Slack and Trello!') });
+
+app.get('/list', function (req, res) {
+    trello.get('/1/lists/' + listId, function (err, data) {
+        if (err) throw err;
+        console.log(data);
+
+        var name = data.name;
+        var url = data.shortUrl;
+        res.status(200).send('Card "' + name + '" created here: <' + url + '>');
+    });
+});
 
 // error handler
 app.use(function (err, req, res, next) {
